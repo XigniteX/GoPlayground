aa2ec1b14c6003bd2bd06a3cc4ad600762fe94dc
Oskar Albers <o.albers@blokkerholding.nl>
2017-04-05 07:44:00 +0200

    Added Gin

---
+++ Playground/src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	rout := Testje()
+	fmt.Printf(rout)
+
+	//router2 := NewRouter2()
+
+	//log.Fatal(http.ListenAndServe(":8080", router2))
+}
+
+
+
+
